b94c3ddd1378802ac4ca76c78bf167182cf3624a
Joseph-Lee98 <josephlee498@gmail.com>
2024-06-16 11:03:18 +0100

    this commit will refactor tests to be more succinct and efficient

--- __tests__/api_tests/api.test.js
+++ __tests__/api_tests/api.test.js
@@ -102,7 +102,7 @@ describe('GET - /api/articles/:article_id',()=>{
     })
 })
 
-describe.only('GET - /api/articles',()=>{
+describe('GET - /api/articles',()=>{
     test('200: When no topic query, should return 200 status code and correct array of article objects with comment_count property',()=>{
         return request(app)
         .get('/api/articles')
