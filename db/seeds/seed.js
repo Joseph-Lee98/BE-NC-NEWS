3b6a826b085c55a768a360b2da5800dd3474c6ba
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-28 21:55:13 +0100

    seed the database used hashed passwords

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -4,118 +4,108 @@ const {
   convertTimestampToDate,
   createRef,
   formatComments,
+  hashPasswords,
 } = require("../../utils/seedUtils");
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db
-    .query(`DROP TABLE IF EXISTS comments;`)
-    .then(() => {
-      return db.query(`DROP TABLE IF EXISTS articles;`);
-    })
-    .then(() => {
-      return db.query(`DROP TABLE IF EXISTS users;`);
-    })
-    .then(() => {
-      return db.query(`DROP TABLE IF EXISTS topics;`);
-    })
-    .then(() => {
-      const topicsTablePromise = db.query(`
-      CREATE TABLE topics (
-        slug VARCHAR PRIMARY KEY,
-        description VARCHAR
-      );`);
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS articles;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+  await db.query(`DROP TABLE IF EXISTS topics;`);
 
-      const usersTablePromise = db.query(`
-      CREATE TABLE users (
-        username VARCHAR(20) PRIMARY KEY,
-        name(30) VARCHAR NOT NULL,
-        avatar_url VARCHAR,
-        password VARCHAR(30) NOT NULL
-      );`);
+  const topicsTablePromise = db.query(`
+    CREATE TABLE topics (
+      slug VARCHAR PRIMARY KEY,
+      description VARCHAR
+    );`);
 
-      return Promise.all([topicsTablePromise, usersTablePromise]);
-    })
-    .then(() => {
-      return db.query(`
-      CREATE TABLE articles (
-        article_id SERIAL PRIMARY KEY,
-        title VARCHAR NOT NULL,
-        topic VARCHAR NOT NULL REFERENCES topics(slug),
-        author VARCHAR NOT NULL REFERENCES users(username),
-        body VARCHAR NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW(),
-        votes INT DEFAULT 0 NOT NULL,
-        article_img_url VARCHAR DEFAULT 'https://images.pexels.com/photos/97050/pexels-photo-97050.jpeg?w=700&h=700'
-      );`);
-    })
-    .then(() => {
-      return db.query(`
-      CREATE TABLE comments (
-        comment_id SERIAL PRIMARY KEY,
-        body VARCHAR NOT NULL,
-        article_id INT REFERENCES articles(article_id) NOT NULL,
-        author VARCHAR REFERENCES users(username) NOT NULL,
-        votes INT DEFAULT 0 NOT NULL,
-        created_at TIMESTAMP DEFAULT NOW()
-      );`);
-    })
-    .then(() => {
-      const insertTopicsQueryStr = format(
-        "INSERT INTO topics (slug, description) VALUES %L;",
-        topicData.map(({ slug, description }) => [slug, description])
-      );
-      const topicsPromise = db.query(insertTopicsQueryStr);
+  const usersTablePromise = db.query(`
+    CREATE TABLE users (
+      username VARCHAR(20) PRIMARY KEY,
+      name VARCHAR(30) NOT NULL,
+      avatar_url VARCHAR,
+      password VARCHAR(30) NOT NULL
+    );`);
 
-      const insertUsersQueryStr = format(
-        "INSERT INTO users ( username, name, avatar_url, password) VALUES %L;",
-        userData.map(({ username, name, avatar_url, password }) => [
-          username,
-          name,
-          avatar_url,
-          password,
-        ])
-      );
-      const usersPromise = db.query(insertUsersQueryStr);
+  await Promise.all([topicsTablePromise, usersTablePromise]);
 
-      return Promise.all([topicsPromise, usersPromise]);
-    })
-    .then(() => {
-      const formattedArticleData = articleData.map(convertTimestampToDate);
-      const insertArticlesQueryStr = format(
-        "INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *;",
-        formattedArticleData.map(
-          ({
-            title,
-            topic,
-            author,
-            body,
-            created_at,
-            votes = 0,
-            article_img_url,
-          }) => [title, topic, author, body, created_at, votes, article_img_url]
-        )
-      );
+  await db.query(`
+    CREATE TABLE articles (
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR NOT NULL,
+      topic VARCHAR NOT NULL REFERENCES topics(slug),
+      author VARCHAR NOT NULL REFERENCES users(username),
+      body VARCHAR NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      votes INT DEFAULT 0 NOT NULL,
+      article_img_url VARCHAR DEFAULT 'https://images.pexels.com/photos/97050/pexels-photo-97050.jpeg?w=700&h=700'
+    );`);
 
-      return db.query(insertArticlesQueryStr);
-    })
-    .then(({ rows: articleRows }) => {
-      const articleIdLookup = createRef(articleRows, "title", "article_id");
-      const formattedCommentData = formatComments(commentData, articleIdLookup);
+  await db.query(`
+    CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      body VARCHAR NOT NULL,
+      article_id INT REFERENCES articles(article_id) NOT NULL,
+      author VARCHAR REFERENCES users(username) NOT NULL,
+      votes INT DEFAULT 0 NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW()
+    );`);
 
-      const insertCommentsQueryStr = format(
-        "INSERT INTO comments (body, author, article_id, votes, created_at) VALUES %L;",
-        formattedCommentData.map(
-          ({ body, author, article_id, votes = 0, created_at }) => [
-            body,
-            author,
-            article_id,
-            votes,
-            created_at,
-          ]
-        )
-      );
-      return db.query(insertCommentsQueryStr);
-    });
+  const hashedUserData = await hashPasswords(userData);
+
+  const insertTopicsQueryStr = format(
+    "INSERT INTO topics (slug, description) VALUES %L;",
+    topicData.map(({ slug, description }) => [slug, description])
+  );
+  const topicsPromise = db.query(insertTopicsQueryStr);
+
+  const insertUsersQueryStr = format(
+    "INSERT INTO users (username, name, avatar_url, password) VALUES %L;",
+    hashedUserData.map(({ username, name, avatar_url, password }) => [
+      username,
+      name,
+      avatar_url,
+      password,
+    ])
+  );
+  const usersPromise = db.query(insertUsersQueryStr);
+
+  await Promise.all([topicsPromise, usersPromise]);
+
+  const formattedArticleData = articleData.map(convertTimestampToDate);
+  const insertArticlesQueryStr = format(
+    "INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *;",
+    formattedArticleData.map(
+      ({
+        title,
+        topic,
+        author,
+        body,
+        created_at,
+        votes = 0,
+        article_img_url,
+      }) => [title, topic, author, body, created_at, votes, article_img_url]
+    )
+  );
+
+  const { rows: articleRows } = await db.query(insertArticlesQueryStr);
+
+  const articleIdLookup = createRef(articleRows, "title", "article_id");
+  const formattedCommentData = formatComments(commentData, articleIdLookup);
+
+  const insertCommentsQueryStr = format(
+    "INSERT INTO comments (body, author, article_id, votes, created_at) VALUES %L;",
+    formattedCommentData.map(
+      ({ body, author, article_id, votes = 0, created_at }) => [
+        body,
+        author,
+        article_id,
+        votes,
+        created_at,
+      ]
+    )
+  );
+  await db.query(insertCommentsQueryStr);
 };
 
 module.exports = seed;
