2542420baa35ff127469ff1b8033da1f4dbd664c
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-29 20:28:19 +0100

    add checkUser function to auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+
+// Define role hierarchy
+const rolesHierarchy = {
+  user: ["user", "admin"],
+  admin: ["admin"],
+};
+
+exports.checkRole = (role) => {
+  return (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "Access denied" });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+
+      const userRole = decoded.role;
+
+      if (!rolesHierarchy[role].includes(userRole)) {
+        return res.status(403).json({ message: "Forbidden" });
+      }
+
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Invalid token" });
+    }
+  };
+};
+
+exports.checkUser = (req, res, next) => {
+  const { username } = req.params; // Assumes the route has a `username` parameter
+  const userRole = req.user.role; // req.user is already populated by checkRole
+
+  // Check if the username in the token matches the one in the request parameters or if the user is an admin
+  if (req.user.username !== username && userRole !== "admin") {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+
+  next();
+};
