cf05985e9f71baead798adfc0fe125ce1e08a617
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-29 11:47:38 +0100

    add hashed password for each user in the dev database

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -4,35 +4,41 @@ module.exports = [
     name: "Tom Tickle",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
+    password: "Tickle@2024#",
   },
   {
     username: "grumpy19",
     name: "Paul Grump",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
+    password: "Grumpy19@#2024",
   },
   {
     username: "happyamy2016",
     name: "Amy Happy",
     avatar_url:
       "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
+    password: "H@ppyAmy2024!",
   },
   {
     username: "cooljmessy",
     name: "Peter Messy",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
+    password: "C00l_Jm3ssy#23",
   },
   {
     username: "weegembump",
     name: "Gemma Bump",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553",
+    password: "W33gemBump@2024",
   },
   {
     username: "jessjelly",
     name: "Jess Jelly",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
+    password: "J3ss_J3lly#21",
   },
 ];
