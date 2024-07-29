5c862619aed52cb24dd6f2af99282954372d72ac
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-29 22:19:05 +0100

    add middleware to check no valid token in header when logging in

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/users.controller";
+import { preventLoggedInUser } from "../middleware/auth";
 
 export const usersRouter = Router();
 
 usersRouter.route("/").post(registerUser);
 
-usersRouter.route("/login").post(loginUser);
+usersRouter.route("/login").post(preventLoggedInUser, loginUser);
