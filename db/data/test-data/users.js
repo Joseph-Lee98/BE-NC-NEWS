44d66ddba0a4f56a7a06004ba2ca7637a01f7925
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-29 22:44:21 +0100

    introduce role property to users table and seed with admin user

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -5,18 +5,21 @@ module.exports = [
     avatar_url:
       "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
     password: "P@ssw0rd_Br1dge!",
+    role: "user",
   },
   {
     username: "icellusedkars",
     name: "sam",
     avatar_url: "https://avatars2.githubusercontent.com/u/24604688?s=460&v=4",
     password: "Ic3ll@usedK@rs2024",
+    role: "user",
   },
   {
     username: "rogersop",
     name: "paul",
     avatar_url: "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
     password: "R0g3r_S0p#123",
+    role: "user",
   },
   {
     username: "lurker",
@@ -24,5 +27,6 @@ module.exports = [
     avatar_url:
       "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
     password: "Lurk3r_D0_N0th1ng!",
+    role: "user",
   },
 ];
