91d0c41efb7e8ed91dfc38bde4fcc81ac8cfef1b
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-29 21:53:24 +0100

    add functionality in model to verify user credentials

--- models/users.model.js
+++ models/users.model.js
@@ -1,1 +1,15 @@
+const db = require("../db/connection");
+const bcrypt = require("bcrypt");
+
 exports.createUser = (req, res, next) => {};
+
+exports.verifyUser = async (username, password) => {
+  const result = await db.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  const user = result.rows[0];
+  if (user && (await bcrypt.compare(password, user.password))) {
+    return user;
+  }
+  return null;
+};
