fd8d19221e8f83f64e1cbf8ba2727d29ed794aa9
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-29 21:35:32 +0100

    add controller functionality for logging user in

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,22 @@
-const { createUser } = require("../models/users.model");
+const { createUser, verifyUser } = require("../models/users.model");
+const jwt = require("jsonwebtoken");
 
 exports.registerUser = (req, res, next) => {};
 
-exports.loginUser = (req, res, next) => {};
+exports.loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await verifyUser(username, password);
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign(
+      { username: user.username, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
