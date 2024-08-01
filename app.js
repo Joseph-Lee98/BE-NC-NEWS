7dc83203e85b235aabe5d3ce5ba2103bfb68520e
Joseph-Lee98 <josephlee498@gmail.com>
2024-08-01 21:15:30 +0100

    remove generateToken util function since now redundant

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   handleCustomErrors,
   notFoundHandler,
+  handleServerErrors,
 } = require("./middleware/errorHandling.js");
 const app = express();
 const apiRouter = require("./routes/api-router.js");
@@ -9,4 +10,5 @@ app.use(express.json());
 app.use("/api", apiRouter);
 app.all("*", notFoundHandler);
 app.use(handleCustomErrors);
+app.use(handleServerErrors);
 module.exports = app;
