ce73ed497bfb2c1e07120efe05ffac83797f0024
Joseph-Lee98 <josephlee498@gmail.com>
2024-08-11 20:06:54 +0100

    refactor controller and test for get endpoints api for consistent responses and more robust testing

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,7 +1,7 @@
 exports.getEndpoints = (req, res, next) => {
   try {
     const endpoints = require("../endpoints.json");
-    res.status(200).send({ endpoints });
+    res.status(200).send(endpoints);
   } catch (err) {
     next(err);
   }
